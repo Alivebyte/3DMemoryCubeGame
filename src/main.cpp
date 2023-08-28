ef1e00faabf88176ea80f9e81ef9504d301b82a8
Alivebyte <alivebyte.yt@gmail.com>
2023-08-28 15:31:31 +0300

    Exit the game

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ class Game : public olc::PixelGameEngine
 public:
 	Game()
 	{
-		sAppName = "Memory ?";
+		sAppName = "Memory";
 	}
 private:
 	void SetMouseView(float fElapsedTime)
@@ -59,39 +59,8 @@ public:
 		}
 		spr_test = new olc::Sprite("content/textures/dirtblock.png");
 
-		//cubeMesh.tris =
-		//{
-		//	// SOUTH
-		//	{ 0.0f, 0.0f, 0.0f, 1.0f,	    0.0f, 1.0f, 0.0f, 1.0f,		 1.0f, 1.0f, 0.0f, 1.0f,	0.0f, 1.0f, 0.0f,	0.0f, 0.0f, 0.0f, 		1.0f, 0.0f, 0.0f, },
-		//	{ 0.0f, 0.0f, 0.0f, 1.0f,  		1.0f, 1.0f, 0.0f, 1.0f,		 1.0f, 0.0f, 0.0f, 1.0f,	0.0f, 1.0f, 0.0f,	1.0f, 0.0f, 0.0f, 		1.0f, 1.0f, 0.0f, },
-
-		//	// EAST
-		//	{ 1.0f, 0.0f, 0.0f, 1.0f,		1.0f, 1.0f, 0.0f, 1.0f,		1.0f, 1.0f, 1.0f, 1.0f,		0.0f, 1.0f, 0.0f,	0.0f, 0.0f, 0.0f, 		1.0f, 0.0f, 0.0f, },
-		//	{ 1.0f, 0.0f, 0.0f, 1.0f,		1.0f, 1.0f, 1.0f, 1.0f,		1.0f, 0.0f, 1.0f, 1.0f,		0.0f, 1.0f, 0.0f,	1.0f, 0.0f, 0.0f, 		1.0f, 1.0f, 0.0f, },
-
-		//	// NORTH
-		//	{ 1.0f, 0.0f, 1.0f, 1.0f,		1.0f, 1.0f, 1.0f, 1.0f,		0.0f, 1.0f, 1.0f, 1.0f,		0.0f, 1.0f, 0.0f,	0.0f, 0.0f, 0.0f, 		1.0f, 0.0f, 0.0f, },
-		//	{ 1.0f, 0.0f, 1.0f, 1.0f,		0.0f, 1.0f, 1.0f, 1.0f,		0.0f, 0.0f, 1.0f, 1.0f,		0.0f, 1.0f, 0.0f,	1.0f, 0.0f, 0.0f, 		1.0f, 1.0f, 0.0f, },
-
-		//	// WEST
-		//	{ 0.0f, 0.0f, 1.0f, 1.0f,		0.0f, 1.0f, 1.0f, 1.0f,		0.0f, 1.0f, 0.0f, 1.0f,		0.0f, 1.0f, 0.0f,	0.0f, 0.0f, 0.0f, 		1.0f, 0.0f, 0.0f, },
-		//	{ 0.0f, 0.0f, 1.0f, 1.0f,		0.0f, 1.0f, 0.0f, 1.0f,		0.0f, 0.0f, 0.0f, 1.0f,		0.0f, 1.0f, 0.0f,	1.0f, 0.0f, 0.0f, 		1.0f, 1.0f, 0.0f, },
-
-		//	// TOP             																 				    
-		//	{ 0.0f, 1.0f, 0.0f, 1.0f,		0.0f, 1.0f, 1.0f, 1.0f,		1.0f, 1.0f, 1.0f, 1.0f,		0.0f, 1.0f, 0.0f,	0.0f, 0.0f, 0.0f, 		1.0f, 0.0f, 0.0f, },
-		//	{ 0.0f, 1.0f, 0.0f, 1.0f,		1.0f, 1.0f, 1.0f, 1.0f,		1.0f, 1.0f, 0.0f, 1.0f,		0.0f, 1.0f, 0.0f,	1.0f, 0.0f, 0.0f, 		1.0f, 1.0f, 0.0f, },
-
-		//	// BOTTOM         																 				   
-		//	{ 1.0f, 0.0f, 1.0f, 1.0f,		0.0f, 0.0f, 1.0f, 1.0f,		0.0f, 0.0f, 0.0f, 1.0f,		0.0f, 1.0f, 0.0f,	0.0f, 0.0f, 0.0f, 		1.0f, 0.0f, 0.0f, },
-		//	{ 1.0f, 0.0f, 1.0f, 1.0f,		0.0f, 0.0f, 0.0f, 1.0f,		1.0f, 0.0f, 0.0f, 1.0f,		0.0f, 1.0f, 0.0f,	1.0f, 0.0f, 0.0f, 		1.0f, 1.0f, 0.0f, },
-
-		//};
-
 		olc::GFX3D::ConfigureDisplay();
 		// Projection stuff
-		//vCamera = { 0.0f, 0.0f, -2.0f };
-		//vLookDir = { 0.0f, 0.0f, 1.0f };
-		
 		pipe.SetProjection(60.0f, (float)ScreenHeight()/(float)ScreenWidth(), 0.1f, 1000.0f, 0.0f, 0.0f, ScreenWidth(), ScreenHeight());
 		return true;
 	}
@@ -99,30 +68,8 @@ public:
 	bool OnUserUpdate(float fElapsedTime)
 	{
 		
-		fTheta += fElapsedTime * 10;
-		/*for (int x = 0; x != ScreenWidth(); x++)
-		{
-			for (int y = 0; y != ScreenHeight(); y++)
-			{
-				Draw(x,y , olc::Pixel(rand() % 255, rand() % 255, rand() % 255));
-			}
-		}*/
-		//olc::vf2d textPos = { 0.0f, 0.0f };
-		//DrawString(textPos, "TEST!!!");
-		
 		Clear(olc::CYAN);
 		olc::GFX3D::ClearDepth();
-
-		//if (GetKey(olc::Key::W).bHeld) vCamera.z += 5.0f * fElapsedTime;
-		//if (GetKey(olc::Key::S).bHeld) vCamera.z -= 5.0f * fElapsedTime;
-		//if (GetKey(olc::Key::D).bHeld) vCamera.x += 5.0f * fElapsedTime;
-		//if (GetKey(olc::Key::A).bHeld) vCamera.x -= 5.0f * fElapsedTime;
-
-
-		
-		/*if (GetKey(olc::Key::Q).bHeld) vLookDir.x -= 5.0f * fElapsedTime;
-		if (GetKey(olc::Key::E).bHeld) vLookDir.x += 5.0f * fElapsedTime;*/
-
 		
 
 		olc::GFX3D::vec3d vForward = olc::GFX3D::Math::Vec_Mul(vLookDir, 5.0f * fElapsedTime);
@@ -132,6 +79,7 @@ public:
 		if (GetKey(olc::Key::S).bHeld) vCamera = olc::GFX3D::Math::Vec_Sub(vCamera, vForward);
 		if (GetKey(olc::Key::D).bHeld) vCamera = olc::GFX3D::Math::Vec_Sub(vCamera, vRight);
 		if (GetKey(olc::Key::A).bHeld) vCamera = olc::GFX3D::Math::Vec_Add(vCamera, vRight);
+		if (GetKey(olc::Key::ESCAPE).bReleased) exit(EXIT_SUCCESS);
 
 		// Temporal keyboard rotation
 		if (GetKey(olc::Key::UP).bHeld) fPitch += 5.0f * fElapsedTime;
@@ -142,19 +90,10 @@ public:
 		// Mouse input
 		SetMouseView(fElapsedTime);
 		
-
-		/*if (fPitch > 1.55f)
-			fPitch = 1.55f;
-		if (fPitch < -1.55f)
-			fPitch = -1.55f;*/
 		fPitch = std::clamp(fPitch, -1.55f, 1.55f);
 
 		if (GetKey(olc::Key::SPACE).bHeld) vCamera = olc::GFX3D::Math::Vec_Sub(vCamera, vUp);
 		if (GetKey(olc::Key::C).bHeld) vCamera = olc::GFX3D::Math::Vec_Add(vCamera, vUp);
-		
-		//std::cout << "X: " << vCamera.x << " Y: " << vCamera.y << " Z: " << vCamera.z << std::endl;
-
-		//std::cout << "Pitch: " << fPitch << std::endl;
 
 		olc::GFX3D::vec3d vLookTarget = { 0, 0, 1 };
 		olc::GFX3D::mat4x4 matRotY = olc::GFX3D::Math::Mat_MakeRotationY(fYaw);
@@ -166,11 +105,6 @@ public:
 		
 		pipe.SetCamera(vCamera, vLookTarget, vUp);
 
-		//olc::GFX3D::mat4x4 matRotX = olc::GFX3D::Math::Mat_MakeRotationX(fTheta);
-		//olc::GFX3D::mat4x4 matRotY = olc::GFX3D::Math::Mat_MakeRotationY(fTheta);
-		//olc::GFX3D::mat4x4 matRotZ = olc::GFX3D::Math::Mat_MakeRotationZ(fTheta / 3.0f);
-
-
 		olc::GFX3D::mat4x4 matWorld = olc::GFX3D::Math::Mat_MakeIdentity();
 
 		pipe.SetTransform(matWorld);
@@ -178,35 +112,6 @@ public:
 		pipe.SetTexture(spr_test);
 
 		pipe.Render(cubeMesh.tris);
-		//pipe.Render(cubeMesh.tris);
-
-		//olc::GFX3D::vec3d vForward = olc::GFX3D::Math::Vec_Mul(vLookDir, 8.0f * fElapsedTime);
-		 
-		//if (GetKey(olc::Key::W).bHeld) vCamera = olc::GFX3D::Math::Vec_Add(vCamera, vForward);
-		//if (GetKey(olc::Key::S).bHeld) vCamera = olc::GFX3D::Math::Vec_Sub(vCamera, vForward);
-		////if (GetKey(olc::Key::W).bHeld) fElapsedTime * (vCamera.z += 1.0f);
-		////if (GetKey(olc::Key::S).bHeld) fElapsedTime * (vCamera.z += -1.0f);
-		//if (GetKey(olc::Key::A).bHeld) fYaw += -2.0f;
-		//if (GetKey(olc::Key::D).bHeld) fYaw += 2.0f;
-
-		////olc::GFX3D::vec3d camPos = { 0.0f, 0.0f, -2.0f };
-		////olc::GFX3D::vec3d lookAt = { 0.0f, 0.0f, 1.0f };
-
-		//olc::GFX3D::vec3d vecUp = { 0.0f, 1.0f, 0.0f };
-		//olc::GFX3D::vec3d vecTarget = { 0, 0, 1.0f };
-		//olc::GFX3D::mat4x4 matCamRot = olc::GFX3D::Math::Mat_MakeRotationY(fYaw);
-		//vLookDir = olc::GFX3D::Math::Mat_MultiplyVector(matCamRot, vLookDir);
-		//vecTarget = olc::GFX3D::Math::Vec_Add(vCamera, vLookDir);
-		//olc::GFX3D::mat4x4 matTransform = olc::GFX3D::Math::Mat_MakeTranslation(0.0f, 0.0f, -2.0f);
-
-		//pipe.SetTransform(matTransform);
-		//// Camera stuff
-		//pipe.SetCamera(vCamera, vecTarget, vecUp);
-		//
-		//// Render stuff
-		////pipe.Render(cubeMesh.tris, olc::GFX3D::RENDERFLAGS::RENDER_WIRE);
-		//olc::GFX3D::vec3d circle = { 0.0f, 0.0f, -2.0f };
-		//pipe.RenderCircleXZ(circle, 4.0f);
 
 		return true;
 		
@@ -238,7 +143,7 @@ private:
 int main()
 {
 	Game game;
-	if (game.Construct(768, 480, 1, 1, true, false))
+	if (game.Construct(768, 480, 1, 1, false, false))
 	{
 		SetCursor(NULL);
 		ShowCursor(FALSE);
